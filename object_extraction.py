b76a8029954b47de179b30a0524c61369ed65fb4
elgandara <eliazargandara@gmail.com>
2015-02-25 15:05:30 -0800

    Python file for project created

---
+++ object_extraction.py
@@ -0,0 +1,13 @@
+"""
+# Project: Image Extraction
+# Description: The program will allow the user to color a region
+#              from an image and then will replace the pixels in
+#              that area with what it estimates would be in that
+#              location of the image.
+#
+# Contributor: Salvador Ramirez
+# Contributor: Eliasar Gandara
+#
+# Date Created: 2/25/15
+# Last Modified: 2/25/15
+"""
