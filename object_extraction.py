e491189141cdadaf58349d9c5aca1dbf0298700d
elgandara <eliazargandara@gmail.com>
2015-02-27 15:34:19 -0800

    I made a change

--- object_extraction.py
+++ object_extraction.py
@@ -1,5 +1,6 @@
 """
-# Project: Image Extraction
+# Project: Object Extraction
+# Filename: object_extraction.py
 # Description: The program will allow the user to color a region
 #              from an image and then will replace the pixels in
 #              that area with what it estimates would be in that
